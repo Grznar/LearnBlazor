359fca5f1aee4c4d04d538adf72555f03ebe18c0
Jakub Černý <AsKronak@seznam.cz>
2025-02-18 09:35:28 +0100

    Routing, Pages, Components , Child, Event, GrandChild, Cascade, Parameters

---
+++ LearnBlazor/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace LearnBlazor.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        
+        public double Price { get; set; }
+        public IEnumerable<ProductProp> ProductProperties { get; set; }
+    }
+}
