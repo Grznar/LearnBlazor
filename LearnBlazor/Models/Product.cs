aa6b800b12ad85021d07301c093723911147790a
Jakub Černý <AsKronak@seznam.cz>
2025-02-19 06:43:38 +0100

    Forms blazor - validation, summary, input, edit text

--- LearnBlazor/Models/Product.cs
+++ LearnBlazor/Models/Product.cs
@@ -1,12 +1,30 @@
-﻿namespace LearnBlazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnBlazor.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public bool IsActive { get; set; }
-        
+        [Range(1,1000)]
         public double Price { get; set; }
         public IEnumerable<ProductProp> ProductProperties { get; set; }
+
+        public DateOnly AvailableAfter { get; set; }
+        public Category Category { get; set; }
+
+        public Product()
+        {
+            AvailableAfter = DateOnly.FromDateTime(DateTime.Now);
+        }
+    }
+
+    public enum Category
+    {
+        Entree,
+        Apetizer,
+        Dessert
     }
 }
